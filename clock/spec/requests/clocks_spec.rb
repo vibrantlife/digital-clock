c545b833635482b7d92db950776b275aedd00f05
Vibrantlife <nicholesvibrantlife@gmail.com>
2015-05-14 13:00:43 -0700

    added routes and first test

--- clock/spec/requests/clocks_spec.rb
+++ clock/spec/requests/clocks_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Clocks", type: :request do
   describe "GET /clocks" do
-    it "works! (now write some real specs)" do
-      get clocks_path
-      expect(response).to have_http_status(200)
-    end
+    it "display a clock" do
+      visit clocks_path
+      page.should have_content 'M'
   end
 end
